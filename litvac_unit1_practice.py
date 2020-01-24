b822b786602317e460d57bae182a48d71994b331
23litvem <59704628+23litvem@users.noreply.github.com>
2020-01-24 09:37:29 -0500

    Add files via upload

---
+++ litvac_unit1_practice.py
@@ -0,0 +1,65 @@
+#!/usr?bin/env python3
+# Part A
+print("Welcome to Information Technology\n\nProgramming and\nWeb Development")
+print("")
+print("_       ___________\n| |     / /_  __/   |\n| | /| / / / / / /| |\n| |/ |/ / / / / ___ |\n|__/|__/ /_/ /_/  |_|")
+
+
+
+
+
+#Part B
+print("A lightning flash:\nbetween the forest trees\nI have seen water.\n\t-Shiki")
+
+
+
+
+
+print("Computer Science is no more about computers\nthan astronomy is about telescopes.\n\t-Edsger W. Dijkstra")
+
+print("^\n/ \ \n/  \ \n |   |\n|   |\n|#  #|\n____")
+
+#PArt C
+user_name = input("What is your name?")
+
+favorite_animal = input("What is your favorite animal?")
+
+print("Hello", user_name< ". Your favorite animal is the", favorite_animal, ".")
+
+number = input("What is your favorite number")
+
+print(number * 3)
+
+user_name = input("dog room cat")
+print("cat room dog")
+
+
+
+
+
+
+
+print("  ",3,"\n\n")
+print(" ______")
+print("|", "    ", "|")
+print("|", "    ", "|")
+print("|", "    ", "|")
+print(" ______")
+
+#PArt D
+
+use_name = input("Manny Litvac")
+number = input("10: ")
+print(number * 8)
+
+#Part E
+a = input("Enter an animal: ")
+s = input ("Enter a sound")
+e = "E-I-E-I-O"
+ 
+print ("Old Macdonald had a farm, " + e)
+print ("ANd on his farm he had a" + a + "," + e)
+print ("With a " + a + "-" + a + " here and a" + s + "-" + s + " there")
+print ("Here a "+ s+ "-" + e)
+print ("Everywhere a"  + s = "-" + a)
+print ("Old Macdonald had a farm," + e)
